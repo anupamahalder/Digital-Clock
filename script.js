de5e04ce3acaf2f349ab38255914835ed267da74
Anupama_Halder <anupamahalder2022@gmail.com>
2023-08-30 22:12:12 +0530

    Making double digit for hour,minute and second time

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ function showTime(){
     let hour = date.getHours();
     let minute = date.getMinutes();
     let second = date.getSeconds();
+    let session = " AM ";
 
+    //make double digit
+    hour = hour < 10 ? "0"+hour : hour;
+    minute = minute < 10 ? "0"+minute : minute;
+    second = second < 10 ? "0"+second : second;
     //formatting to show time on screen
-    let time = hour + " : " + minute + " : " + second;
+    let time = hour + " : " + minute + " : " + second + session;
     document.getElementById('myTime').innerText = time;
 
     //To refresh the time with each 1000 miliseconds
